b1f73f9b7addd18dd99abd6b42f382e8a34e018e
Elikoam <130307596+Elikoam@users.noreply.github.com>
2023-04-15 21:38:27 +0430

    added icon

--- src/index.js
+++ src/index.js
@@ -30,6 +30,8 @@ function showTemperature(response){
     let humidityElement=document.querySelector("#humidity");
     let windElement=document.querySelector("#wind");
     let dateElement=document.querySelector("#date");
+    let iconElement=document.querySelector("#icon");
+    console.log(response.data.weather);
 
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
     cityElement.innerHTML=response.data.name;
@@ -37,9 +39,11 @@ function showTemperature(response){
     humidityElement.innerHTML=response.data.main.humidity;
     windElement.innerHTML=Math.round(response.data.wind.speed);
     dateElement.innerHTML=formatDate(response.data.dt *1000);
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "34ae1065362d42545661451bda2b8a1f";
-let city ="Tehran";
+let city ="sarab";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemperature);
